7d8dc906f87aaca67ab9c60cbc5dace20cee754a
Akra <Akrapovichpro@gmail.com>
2025-01-16 13:23:27 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version('bodacious')
+game('gta5')
+
+author 'Akra'
+description 'Cradealer'
+version '1.0'
+
+shared_scripts {
+  'config.lua',
+}
+
+client_scripts {
+  'RageUIv6/RMenu.lua',
+	'RageUIv6/menu/RageUI.lua',
+	'RageUIv6/menu/Menu.lua',
+	'RageUIv6/menu/MenuController.lua',
+	'RageUIv6/components/*.lua',
+	'RageUIv6/menu/elements/*.lua',
+	'RageUIv6/menu/items/*.lua',
+	'RageUIv6/menu/panels/*.lua',
+	'RageUIv6/menu/panels/*.lua',
+	'RageUIv6/menu/windows/*.lua',
+  ---------------------
+	------ Client -------
+	---------------------
+  
+}
