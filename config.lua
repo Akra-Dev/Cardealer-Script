f0f5434fc180e0a3399b5a2b738078251ecfe6e5
Akra <Akrapovichpro@gmail.com>
2025-01-16 13:13:55 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {
+   Get = {
+  		Marker = {
+  			Type = 2,
+  			Size = {0.2, 0.2, 0.2},
+  			Color = {130,0,184},
+  			Rotation = 180.0,
+  		},
+    },
+  Concess = {
+		pos = {vector3(-35.2910, -1108.5481, 26.4223)},
+	},
+
+}
+
